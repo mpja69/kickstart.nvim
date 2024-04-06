575ba23c1c2146d2f24bd9d8f7890f1e5249bce5
Magnus Andersson <mpja69@gmail.com>
2024-04-06 21:13:03 +0200

    La in min lokala keymap för toggle

--- lua/custom/plugins/telekasten.lua
+++ lua/custom/plugins/telekasten.lua
@@ -14,6 +14,15 @@ return {
       ['<leader>z'] = { name = '[Z]ettelkasten', _ = 'which_key_ignore' },
     }
 
+    vim.api.nvim_create_autocmd('BufEnter', {
+      pattern = 'ToDo.md',
+      desc = 'Toggle ToDo',
+      group = vim.api.nvim_create_augroup('mpja69_todo', { clear = true }),
+      callback = function(event)
+        vim.keymap.set('n', '<leader><cr>', require('telekasten').toggle_todo, { buffer = event.buf, desc = 'Toggle ToDo' })
+      end,
+    })
+
     vim.keymap.set('n', '<leader>zf', require('telekasten').find_notes, { desc = '[Z]ettelkasten - [F]ind notes' })
     vim.keymap.set('n', '<leader>zd', require('telekasten').goto_today, { desc = '[Z]ettelkasten - [D]aily note' })
     vim.keymap.set('n', '<leader>zg', require('telekasten').search_notes, { desc = '[Z]ettelkasten - [G]rep notes' })
