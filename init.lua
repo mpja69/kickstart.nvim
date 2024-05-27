d7a1edfb086de84b526fc0124d69798ec8cf23e7
Magnus Andersson <mpja69@gmail.com>
2024-05-27 21:31:37 +0200

    La till pyright och tsserver

--- init.lua
+++ init.lua
@@ -454,7 +454,7 @@ require('lazy').setup({
       local servers = {
         -- clangd = {},
         -- gopls = {},
-        -- pyright = {},
+        pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
@@ -462,7 +462,7 @@ require('lazy').setup({
         --    https://github.com/pmizio/typescript-tools.nvim
         --
         -- But for many setups, the LSP (`tsserver`) will work just fine
-        -- tsserver = {},
+        tsserver = {},
         --
 
         lua_ls = {
