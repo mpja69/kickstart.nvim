b3796abbca617465a5a7f46a8548344af95f3753
Magnus Andersson <mpja69@gmail.com>
2024-07-22 10:00:34 +0200

    Added support for TypeScript

--- lua/kickstart/plugins/debug.lua
+++ lua/kickstart/plugins/debug.lua
@@ -21,10 +21,78 @@ return {
     -- Add your own debuggers here
     -- 'leoluz/nvim-dap-go',
     'mfussenegger/nvim-dap-python',
+    --mpja69: ------------ js -----------------
+    'mxsdev/nvim-dap-vscode-js',
+    {
+      'microsoft/vscode-js-debug',
+      version = '1.x',
+      build = 'npm i && npm run compile vsDebugServerBundle && mv dist out',
+    },
   },
   config = function()
     local dap = require 'dap'
     local dapui = require 'dapui'
+    --mpja69: ------------ js -----------------
+    require('dap-vscode-js').setup {
+      debugger_path = vim.fn.stdpath 'data' .. '/lazy/vscode-js-debug',
+      adapters = { 'pwa-node', 'pwa-chrome', 'pwa-msedge', 'node-terminal', 'pwa-extensionHost' },
+    }
+    for _, language in ipairs { 'typescript', 'javascript', 'svelte' } do
+      require('dap').configurations[language] = {
+        -- attach to a node process that has been started with
+        -- `--inspect` for longrunning tasks or `--inspect-brk` for short tasks
+        -- npm script -> `node --inspect-brk ./node_modules/.bin/vite dev`
+        {
+          -- use nvim-dap-vscode-js's pwa-node debug adapter
+          type = 'pwa-node',
+          -- attach to an already running node process with --inspect flag
+          -- default port: 9222
+          request = 'attach',
+          -- allows us to pick the process using a picker
+          processId = require('dap.utils').pick_process,
+          -- name of the debug action you have to select for this config
+          name = 'Attach debugger to existing `node --inspect` process',
+          -- for compiled languages like TypeScript or Svelte.js
+          sourceMaps = true,
+          -- resolve source maps in nested locations while ignoring node_modules
+          resolveSourceMapLocations = {
+            '${workspaceFolder}/**',
+            '!**/node_modules/**',
+          },
+          -- path to src in vite based projects (and most other projects as well)
+          cwd = '${workspaceFolder}/src',
+          -- we don't want to debug code inside node_modules, so skip it!
+          skipFiles = { '${workspaceFolder}/node_modules/**/*.js' },
+        },
+        {
+          type = 'pwa-chrome',
+          name = 'Launch Chrome to debug client',
+          request = 'launch',
+          url = 'http://localhost:5173',
+          sourceMaps = true,
+          protocol = 'inspector',
+          port = 9222,
+          webRoot = '${workspaceFolder}/src',
+          -- skip files from vite's hmr
+          skipFiles = { '**/node_modules/**/*', '**/@vite/*', '**/src/client/*', '**/src/*' },
+        },
+        -- only if language is javascript, offer this debug action
+        language == 'javascript'
+            and {
+              -- use nvim-dap-vscode-js's pwa-node debug adapter
+              type = 'pwa-node',
+              -- launch a new process to attach the debugger to
+              request = 'launch',
+              -- name of the debug action you have to select for this config
+              name = 'Launch file in new node process',
+              -- launch current file
+              program = '${file}',
+              cwd = '${workspaceFolder}',
+            }
+          or nil,
+      }
+    end
+    --mpja69: ---------------------------------
 
     require('mason-nvim-dap').setup {
       -- Makes a best effort to setup the various debuggers with
@@ -86,10 +154,10 @@ return {
     -- Install golang specific config
     -- require('dap-go').setup {
     --   delve = {
-        -- On Windows delve must be run attached or it crashes.
-        -- See https://github.com/leoluz/nvim-dap-go/blob/main/README.md#configuring
-          -- detached = vim.fn.has 'win32' == 0,
-      -- },
+    -- On Windows delve must be run attached or it crashes.
+    -- See https://github.com/leoluz/nvim-dap-go/blob/main/README.md#configuring
+    -- detached = vim.fn.has 'win32' == 0,
+    -- },
     -- }
     require('dap-python').setup '~/.virtualenvs/debugpy/bin/python'
   end,
